e896c7b2508e1d4ae0669eab1e6e726a869c3b04
luca590 <lucasgaylord@protonmail.com>
2018-02-25 07:24:31 +0800

    fixed print

--- Lab2/lab2p0.c
+++ Lab2/lab2p0.c
@@ -23,8 +23,7 @@ int main() {
 	int cpid;
 	if((cpid = fork()) != 0) {
 		//Tjis is the parent
-		printf("Hello, I am the PARENT. My PID is %d, my child's PID is %d,
-			   	and my parent's PID is %d\n", getpid(), cpid, getppid());
+		printf("Hello, I am the PARENT. My PID is %d, my child's PID is and my parent's PID is %d\n", getpid(), cpid, getppid());
 	}
 	else {
 		printf("Hello, I am the CHILD. My PID is %d, my parent's PID is %d, for returned %d.\n",
